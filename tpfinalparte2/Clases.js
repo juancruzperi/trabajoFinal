8a2a373cc63bcea843018e9f61f66612440124b1
hannamendoza <hannachantalmendoza18@gmail.com>
2025-11-07 21:47:06 -0300

    Add files via upload

--- tpfinalparte2/Clases.js
+++ tpfinalparte2/Clases.js
@@ -1,14 +1,14 @@
-//Agregar condicion de tiempo, si no llegas a la salida antes del tiempo, perdes
-
-
 class Juego{
   
-//constructor(){
-//this.Ann;
-//this.Cecy;
-//this.Hechizos;
-//this.vidas=3;
-//}
+constructor(){
+//this.Ann = new Ann();
+this.Cecy = new Cecy();
+}
+
+mostrar(){
+  this.Cecy.mostrar();
+}
+
 iniciar(){
 }
 
@@ -23,59 +23,94 @@ reiniciar(){
 
 }
 
-class Ann{//(Jugador)
+//class Ann{//(Jugador)
   
-constructor(){
-this.px=0;
-this.py=0;
-this.vidas=3;
-}
-moverIzquierda(){
-  this.px -=5;
-}
+//constructor(){
+//this.px=0;
+//this.py=0;
+//this.vidas=3;
+//}
 
-moverDerecha(){
-  this.px +=5;
-}
+//moverX(){
+//  if (KeyCode===LEFT_ARROW){
+//  this.px --;
+//}
+  
+//  if (KeyCode===RIGHT_ARROW){
+//  this.px ++;
+//}
+//}
 
-moverArriba(){
-  this.py -=5;
-}
+//moverY(){
+//  if (KeyCode===UP_ARROW){
+//  this.py --;
+//  }
+//  if (KeyCode===DOWN_ARROW){
+//  this.py ++;
+//  }
+//}
 
-moverAbajo(){
-  this.py +=5;
-}
+//vidas(){
+//  this.vidas=3;
+//  if (this.vidas = 0){ //no se si va asi 
+//  Juego.perder();
+//  }
+//}
 
-vidas(){
-  this.vidas=3;
-  if (this.vidas = 0){ //no se si va asi 
-  Juego.perder();
-  }
-}
-
-}
+//}
 
 
 
 class Cecy{//(Enemigo)
   
 constructor(){
-this.posx=0;
-this.py=0;
+this.px = 520;
+this.py = 30;
+this.alto = 170;
+this.baja = true;
+this.Hechizo = new Hechizo();
 }
 
-mover(){//la idea es que vaya subiendo y bajando automaticamente, se inicia cuando se clickea la pantalla al comenzar
-}
-
-}
-
-class Hechizos {//los hechizos salen desde la varita de cecy que va a estar en movimiento, lo ideal seria que se disparen automaticamente cada un segundo
-
-constructor(){
-this.px=0;
-this.py=0;
+mostrar(){
+this.mover();
+image (Cecyimg, this.px, this.py, 100,this.alto);
+this.Hechizo.mostrar(this.px-40, this.py+30)
 }
 
 mover(){
+
+  if (this.baja) {
+    this.py += 5;
+  if (this.py >= height - this.alto) {
+    this.baja = false;
+  }
+  }
+  
+  else { this.py -= 5
+  if (this.py <= 30){
+  this.baja = true;
+  }
+  }
 }
+
+}
+
+
+class Hechizo {//los hechizos salen desde la varita de cecy que va a estar en movimiento, lo ideal seria que se disparen automaticamente cada un segundo
+
+constructor(){
+this.px = 470;
+this.py = 50;
+this.baja = true;
+}
+
+mostrar(px,py){
+this.mover();
+image(hechizo, px, py, 70, 30);
+}
+
+mover(){
+
+}
+
 }
--- tpfinalparte2/tpfinalparte2.js
+++ tpfinalparte2/tpfinalparte2.js
@@ -1,13 +1,24 @@
 //Comision 1
 //Hanna Mendoza y Juan Cruz Peri
+let juego;
+let fondo, Annimg, Cecyimg, hechizo;
+let cancion;
+
+function preload (){
+fondo = loadImage('data/fondo.jpg');
+Annimg = loadImage('data/Annimg.png');
+Cecyimg = loadImage('data/Cecyimg.png');
+hechizo = loadImage('data/hechizo.png');
+cancion = loadSound('data/cancion.mp3');
+}
 
 function setup() {
-CreateCanvas(640,480);
-
-
+createCanvas(640,480);
+juego = new Juego();
 }
 
 
 function draw() {
-
+image(fondo, 0,0);
+juego.mostrar();
 }
